2b729bf83d4f82d853f8a4984a06b7021619c54f
s-avery <s_avery@riseup.net>
2022-07-13 17:14:53 -0400

    more pseudocode tweaks

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 //     - make basic html and css
 //     - populate dropdown list with titles of movies
 //     - update html to include image, title, description of the selected movie
+
 // Stretch goals
 //     - randomizer
 //     - filter by runtime / year / etc
 //     - include more info in the appended html(director, rotten tomatoes score, species(?), etc)
 //     - "developer's choice" our fav ghibli movies lol
-//     - "error image"
+//     - error handling
+//          - spinny animation if api call is slow to populate the dropdown
+//          - placeholder text/img if api is missing data 
 
 
 
@@ -18,31 +21,32 @@
 const app = {}
 
 // *Define app init
+app.init = () => {
+    
+}
 
 // *Make API call
-    // * figure out CORS authentication
-    // * this was a proxy server we used in the JS course - https://morning-coast-00478.herokuapp.com/
         // *construct the url
         // *parameters for our api key
 
         // *use fetch to get data
         // *.then #1 - return json data
-        // *.then #2 - call method to populat dropdown
+        // *.then #2 - call method to populate dropdown
 
     // *with those results:
     // *for each: add movie title to dropdown (so it's pre-populated with list of movie titles on pageload)
-        // *declare movie title variable
         // *function to add it to the dropdown list
         // *forEach to, for each movie...
         // *...declare title variable to print to dropdown
         // *append the movie title to the dropdown
 
-    // *add event listener to dropdown menu change
+    // *add event listener to dropdown menu change 
+        // !(OR submit button TBD)
 
         // *clear previous entry (so when you choose a new movie the previous movie's info doesn't stay there)
 
         // *run function to append movie info (poster, title, descrption)
-            // *define variables for movie info
+            // *declare variables for movie info
             // *create innerhtml with matching css to be appended with variables for movie info
             // *append that to the page
 
